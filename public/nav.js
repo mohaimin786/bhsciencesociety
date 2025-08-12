fc481e869b28143398e9f4bebfcffe41ce9b7e29
mohaimin786 <97736072+mohaimin786@users.noreply.github.com>
2025-08-13 02:15:00 +0500

    Update nav.js

--- public/nav.js
+++ public/nav.js
@@ -45,6 +45,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 font-weight: bold;
                 box-shadow: 0 4px 12px rgba(0,255,174,0.4);
                 transition: all 0.3s ease;
+                 margin-left:30px;
+
             `;
             dashboardLink.addEventListener('mouseover', () => {
                 dashboardLink.style.transform = 'scale(1.05)';
@@ -73,6 +75,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 font-weight: bold;
                 box-shadow: 0 4px 12px rgba(255,92,92,0.4);
                 transition: all 0.3s ease;
+                margin-left:30px;
             `;
             logoutLink.addEventListener('mouseover', () => {
                 logoutLink.style.transform = 'scale(1.05)';
